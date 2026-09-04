f3ff4085e8f9affa708c222eefbd666e8b15d4ac
khajaghousem1988-crypto <khajaghousem1988@gmail.com>
2026-09-04 10:19:31 +0530

    Change Terraform backend region to eu-north-1

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "banking-infra"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     dynamodb_table = "terraform-state-lock-dev"
     encrypt        = true
   }
